a0b3f251fbeb8bacb2509add09d0e55ff6d29c20
Ruby <93859705+MisterRuby@users.noreply.github.com>
2023-09-14 21:25:17 +0900

    init : 프로젝트 생성

---
+++ src/main/java/ruby/rubycicd/RubyCicdApplication.java
@@ -0,0 +1,13 @@
+package ruby.rubycicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RubyCicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RubyCicdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ruby/rubycicd/RubyCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package ruby.rubycicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RubyCicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
